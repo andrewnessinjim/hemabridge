84baf25a4c70b2911d1e2d94c2ee6d9e85726ccf
Andrew Nessin <andrewnessinjim@yahoo.com>
2026-08-16 00:42:08 +0530

    Initial Commit

---
+++ src/app/Canvas.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+import useRequestAnimationFrameLoop from "./useRequestAnimationFrameLoop";
+
+type DrawParams = {
+  ctx: CanvasRenderingContext2D;
+  dpr: number;
+  width: number;
+  height: number;
+  deltaTime: number;
+  totalTime: number;
+};
+
+type CanvasProps = React.CanvasHTMLAttributes<HTMLCanvasElement> & {
+  draw: (params: DrawParams) => void;
+};
+
+function Canvas({ draw, ...delegated }: CanvasProps) {
+  const ctxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+  const boundingBoxRef = React.useRef<DOMRect | null>(null);
+  const dprRef = React.useRef<number | null>(null);
+
+  const startTimeRef = React.useRef<number | null>(null);
+  const lastTimeRef = React.useRef<number | null>(null);
+
+  const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
+
+  React.useEffect(() => {
+    startTimeRef.current = Date.now();
+    lastTimeRef.current = Date.now();
+
+    if (!canvasRef.current) {
+      return;
+    }
+
+    // Do initial setup work
+    const ctx = canvasRef.current.getContext("2d");
+    ctxRef.current = ctx;
+
+    // Register event handlers for width/height/dpr:
+    function handleResize() {
+      if (!canvasRef.current) {
+        return;
+      }
+
+      const dpr = window.devicePixelRatio;
+      const bb = canvasRef.current.getBoundingClientRect();
+      dprRef.current = dpr;
+      boundingBoxRef.current = bb;
+
+      canvasRef.current.setAttribute("width", String(bb.width * dpr));
+      canvasRef.current.setAttribute("height", String(bb.height * dpr));
+      ctx?.scale(dpr, dpr);
+    }
+
+    // Set initial values:
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Call the draw function on every frame:
+  useRequestAnimationFrameLoop(() => {
+    if (
+      !ctxRef.current ||
+      !dprRef.current ||
+      !boundingBoxRef.current ||
+      lastTimeRef.current === null ||
+      startTimeRef.current === null
+    ) {
+      return;
+    }
+
+    const ctx = ctxRef.current;
+    const dpr = dprRef.current;
+    const boundingBox = boundingBoxRef.current;
+
+    const now = Date.now();
+    const deltaTime = Math.min(now - lastTimeRef.current, 250) / 1000;
+    const totalTime = now - startTimeRef.current;
+    lastTimeRef.current = now;
+
+    draw({
+      ctx,
+      dpr,
+      width: boundingBox.width,
+      height: boundingBox.height,
+      deltaTime,
+      totalTime,
+    });
+  });
+
+  return <canvas {...delegated} ref={canvasRef} />;
+}
+
+export default Canvas;
---
+++ src/app/useRequestAnimationFrameLoop.ts
@@ -0,0 +1,33 @@
+import React from "react";
+
+const useRequestAnimationFrameLoop = (callback: () => void, isRunning = true) => {
+  const animationFrameId = React.useRef<number | null>(null);
+  const savedCallback = React.useRef(callback);
+
+  React.useEffect(() => {
+    if (!isRunning) {
+      return;
+    }
+
+    const tick = () => {
+      if (typeof savedCallback.current !== "function") {
+        return;
+      }
+
+      savedCallback.current();
+      animationFrameId.current = window.requestAnimationFrame(tick);
+    };
+
+    tick();
+
+    return () => {
+      if (animationFrameId.current) {
+        window.cancelAnimationFrame(animationFrameId.current);
+      }
+    };
+  }, [isRunning]);
+
+  return animationFrameId;
+};
+
+export default useRequestAnimationFrameLoop;
