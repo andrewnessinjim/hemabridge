4863e9e4284ddb99dbf5c50aa700dacb0487cee8
Andrew Nessin <andrewnessinjim@yahoo.com>
2026-08-17 02:48:51 +0530

    Flowing WBCs

--- src/components/BloodVessel/WBC.tsx
+++ src/components/BloodVessel/WBC.tsx
@@ -2,16 +2,28 @@
 
 import React from "react";
 import { CanvasContext } from "@/components/Canvas";
+import { VesselRatiosContext } from "./VesselRatios";
 import { random, range } from "lodash";
-import { convertPolarToCartesian, normalize } from "@/app/utils";
+import {
+  convertPolarToCartesian,
+  normalize,
+  clampedNormalize,
+} from "@/app/utils";
 
 import createNoiseGenerator from "../../vendor/noise.vendor";
 
+const NUM_WBC = 5;
 const NUM_WBC_POINTS = 200;
 const WMC_NOISE_ZOOM = 4;
 
 const NUM_NUCLEUS_POINTS = 20;
 const NUCLEUS_NOISE_ZOOM = 2;
+
+const WBC_RADIUS = 24;
+const Y_VARIATION = 0.0005;
+const ROTATION_SPEED = 0.005; // radians of spin per pixel of forward movement
+
+const { simplex2: driftNoise } = createNoiseGenerator(4321);
 
 function generateBlobPoints(
   numPoints: number,
@@ -20,7 +32,7 @@ function generateBlobPoints(
   noiseZoom: number,
 ) {
   const { simplex2 } = createNoiseGenerator(random(1000));
-  const blobPoints = range(NUM_WBC_POINTS).map((i) => {
+  const blobPoints = range(numPoints).map((i) => {
     const angleRadians = normalize(i, 0, numPoints, 0, 2 * Math.PI);
 
     // Gives a circular dimension for the noise instead of a linear one
@@ -41,55 +53,118 @@ function generateBlobPoints(
   return blobPoints;
 }
 
+function createWBC(cyFraction: number) {
+  const initCx = random(-45, -90);
+  return {
+    points: generateBlobPoints(NUM_WBC_POINTS, 20, 24, WMC_NOISE_ZOOM),
+    nucleusPoints: generateBlobPoints(
+      NUM_NUCLEUS_POINTS,
+      6,
+      8,
+      NUCLEUS_NOISE_ZOOM,
+    ),
+    nucleusAngle: random(0, 2 * Math.PI),
+    nucleusDistance: random(4, 10),
+    cx: initCx,
+    cyFraction,
+    velocity: random(1, 5, true),
+    rotation: random(0, 2 * Math.PI),
+  };
+}
+
+type WBCState = ReturnType<typeof createWBC>;
+
 export default function WBC() {
   const canvasContextValue = React.useContext(CanvasContext);
+  const ratios = React.useContext(VesselRatiosContext);
+
+  const totalParts =
+    2 * (ratios.intima + ratios.media + ratios.adventitia) + ratios.lumen;
+  const wallFraction =
+    (ratios.intima + ratios.media + ratios.adventitia) / totalParts;
+  const lumenMinFraction = wallFraction;
+  const lumenMaxFraction = 1 - wallFraction;
+
+  const WBCsRef = React.useRef(
+    range(NUM_WBC).map(() =>
+      createWBC(random(lumenMinFraction, lumenMaxFraction)),
+    ),
+  );
 
   React.useEffect(() => {
     if (!canvasContextValue) return;
 
-    const wbc = {
-      points: generateBlobPoints(NUM_WBC_POINTS, 20, 24, WMC_NOISE_ZOOM),
-      nucleusPoints: generateBlobPoints(
-        NUM_NUCLEUS_POINTS,
-        6,
-        8,
-        NUCLEUS_NOISE_ZOOM,
-      ),
-      nucleusAngle: random(0, 2 * Math.PI),
-      nucleusDistance: random(4, 10),
-    };
     const { register } = canvasContextValue;
 
-    const unregister = register(({ ctx }) => {
-      ctx.beginPath();
-      const [firstPoint, ...restPoints] = wbc.points;
-      ctx.moveTo(firstPoint.x + 100, firstPoint.y + 100);
+    const unregister = register(
+      ({ ctx, canvasWidth, canvasHeight, totalTime }) => {
+        function drawWBC(wbc: WBCState, cx: number, cy: number) {
+          ctx.save();
+          ctx.translate(cx, cy);
+          ctx.rotate(wbc.rotation);
 
-      restPoints.forEach((point) => {
-        ctx.lineTo(point.x + 100, point.y + 100);
-      });
+          ctx.beginPath();
+          const [firstPoint, ...restPoints] = wbc.points;
+          ctx.moveTo(firstPoint.x, firstPoint.y);
 
-      ctx.fillStyle = "hsl(90deg 100% 100%)";
-      ctx.fill();
+          restPoints.forEach((point) => {
+            ctx.lineTo(point.x, point.y);
+          });
 
-      ctx.beginPath();
-      const [nucleusFirstPoint, ...restNucleusPoints] = wbc.nucleusPoints;
-      const [nucleusX, nucleusY] = convertPolarToCartesian([
-        wbc.nucleusAngle,
-        wbc.nucleusDistance,
-      ]);
-      ctx.moveTo(
-        nucleusFirstPoint.x + 100 + nucleusX,
-        nucleusFirstPoint.y + 100 + nucleusY,
-      );
-      restNucleusPoints.forEach((point) => {
-        ctx.lineTo(point.x + 100 + nucleusX, point.y + 100 + nucleusY);
-      });
-      ctx.fillStyle = "hsl(207deg 55% 55%)";
-      ctx.fill();
-    }, 1);
+          ctx.fillStyle = "hsl(90deg 100% 100%)";
+          ctx.fill();
+
+          ctx.beginPath();
+          const [nucleusFirstPoint, ...restNucleusPoints] = wbc.nucleusPoints;
+          const [nucleusX, nucleusY] = convertPolarToCartesian([
+            wbc.nucleusAngle,
+            wbc.nucleusDistance,
+          ]);
+          ctx.moveTo(
+            nucleusFirstPoint.x + nucleusX,
+            nucleusFirstPoint.y + nucleusY,
+          );
+          restNucleusPoints.forEach((point) => {
+            ctx.lineTo(point.x + nucleusX, point.y + nucleusY);
+          });
+          ctx.fillStyle = "hsl(207deg 55% 55%)";
+          ctx.fill();
+
+          ctx.restore();
+        }
+
+        WBCsRef.current.forEach((wbc) => {
+          drawWBC(wbc, wbc.cx, wbc.cyFraction * canvasHeight);
+          wbc.cx += wbc.velocity;
+          wbc.rotation += wbc.velocity * ROTATION_SPEED;
+
+          const driftMin = Math.max(
+            wbc.cyFraction - Y_VARIATION,
+            lumenMinFraction,
+          );
+          const driftMax = Math.min(
+            wbc.cyFraction + Y_VARIATION,
+            lumenMaxFraction,
+          );
+
+          wbc.cyFraction = clampedNormalize(
+            driftNoise(wbc.cx / 100, totalTime / 100),
+            -1,
+            1,
+            driftMin,
+            driftMax,
+          );
+
+          if (wbc.cx > canvasWidth + WBC_RADIUS) {
+            wbc.cx = random(-45, -90);
+            wbc.cyFraction = random(lumenMinFraction, lumenMaxFraction);
+          }
+        });
+      },
+      1,
+    );
 
     return unregister;
-  }, [canvasContextValue]);
+  }, [canvasContextValue, lumenMinFraction, lumenMaxFraction]);
   return <React.Fragment />;
 }
