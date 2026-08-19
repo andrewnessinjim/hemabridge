f09fa5251a3450c058cdb2c2fad7e4f1e574bed8
Andrew Nessin <andrewnessinjim@yahoo.com>
2026-08-20 00:09:43 +0530

    Animate popup screen

--- src/components/StyledDialog/StyledDialog.tsx
+++ src/components/StyledDialog/StyledDialog.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Dialog, VisuallyHidden } from "radix-ui";
 import styles from "./StyledDialog.module.scss";
+import { motion, Variants } from "motion/react";
 
 type Props = {
   trigger: React.ReactNode;
@@ -9,6 +10,7 @@ type Props = {
   description: string;
 };
 
+const MotionDialogContent = motion.create(Dialog.Content);
 function StyledDialog({ children, trigger, title, description }: Props) {
   return (
     <Dialog.Root>
@@ -16,13 +18,36 @@ function StyledDialog({ children, trigger, title, description }: Props) {
       <Dialog.Portal>
         <Dialog.Overlay className={styles.DialogOverlay} />
         <Dialog.Content className={styles.DialogContent}>
-          <Dialog.Title>
-            <VisuallyHidden.Root>{title}</VisuallyHidden.Root>
-          </Dialog.Title>
-          <Dialog.Description>
-            <VisuallyHidden.Root>{description}</VisuallyHidden.Root>
-          </Dialog.Description>
-          {children}
+          <motion.div
+            className={styles.AnimatedDialogGraphic}
+            initial={{
+              y: -20,
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+          ></motion.div>
+          <motion.div
+            className={styles.AnimatedDialogContent}
+            initial={{
+              y: 20,
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+          >
+            <Dialog.Title>
+              <VisuallyHidden.Root>{title}</VisuallyHidden.Root>
+            </Dialog.Title>
+            <Dialog.Description>
+              <VisuallyHidden.Root>{description}</VisuallyHidden.Root>
+            </Dialog.Description>
+            {children}
+          </motion.div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
